72c3ef78d2e39a50ca73c764d217dac310a625c5
Rafahersing <adm.rafaelah@gmail.com>
2025-10-11 17:35:58 -0300

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,21 +1,29 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { useLocation } from "react-router-dom";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith("/admin");
+
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
+      {/* Exibe Header e Footer apenas fora do admin */}
+      {!isAdmin && <Header />}
 
-      {/* Área principal com limite de largura */}
-      <main className="flex-1 w-full max-w-[1200px] mx-auto px-4">
+      <main
+        className={`flex-1 ${
+          isAdmin ? "" : "max-w-[1200px] mx-auto w-full px-4"
+        }`}
+      >
         {children}
       </main>
 
-      <Footer />
+      {!isAdmin && <Footer />}
     </div>
   );
 };
