fedf7f0c74757408cdf3ce998e82b6dcbeedfebc
Rafahersing <adm.rafaelah@gmail.com>
2025-10-11 12:34:07 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
+// src/main.tsx (MUDANÇA NA IMPORTAÇÃO)
+
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// Antes: import App from "./App.tsx";
+import { App } from "./App.tsx"; // ⭐️ IMPORTAÇÃO NOMEADA ⭐️
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
