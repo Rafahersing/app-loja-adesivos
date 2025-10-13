c289682248acc1368d55f2231c57770de738b7e1
Rafahersing <adm.rafaelah@gmail.com>
2025-10-12 23:44:16 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,11 +2,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Certifique-se de que estas variáveis de ambiente estão definidas no Vercel (ou no seu .env local)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL; 
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY; 
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("As variáveis de ambiente SUPABASE_URL e SUPABASE_ANON_KEY devem ser definidas.");
+  throw new Error("As variáveis de ambiente VITE_PUBLIC_SUPABASE_URL e VITE_PUBLIC_SUPABASE_ANON_KEY devem ser definidas.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
